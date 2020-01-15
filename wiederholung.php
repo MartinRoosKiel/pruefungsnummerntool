72469bc657fd2833b52ab4b26eac0e3f44b2ea93
Martin Roos <Martin.Roos@gmx.at>
2020-01-15 23:19:49 +0100

    removed bugs and smell from wiederholungs.php

--- wiederholung.php
+++ wiederholung.php
@@ -17,18 +17,19 @@ if ( logged_in() ) {
 if(isset($_POST['nachname'])&isset($_POST['vorname'])&isset($_POST['datum'])&isset($_POST['ausbilderId']))
 {
 if(logged_in()){
-	$nachname = $_POST['nachname'];
-	$vorname = $_POST['vorname'];
-	$datum = $_POST['datum'];
-	$ausbilderId = $_POST['ausbilderId'];
+	$nachname = htmlspecialchars($_POST['nachname']);
+	$vorname = htmlspecialchars($_POST['vorname']);
+	$datum = htmlspecialchars($_POST['datum']);
+	$ausbilderId = htmlspecialchars($_POST['ausbilderId']);
  	echo "<br><hr>";
 	echo "letzter Eintrag <br> Name: ".$nachname."<br> Vorname: ";
 	echo $vorname."<br> Datum: ".$datum."<br>";
 	$erg = eintragen_wiederholung($nachname,$vorname,$datum,$ausbilderId);
 	echo "<hr>";
 }
-else
-echo "Sie sind nicht eingeloggt!";
+else{
+	echo "Sie sind nicht eingeloggt!";
+}
 }
 
 
@@ -38,35 +39,26 @@ echo "Sie sind nicht eingeloggt!";
 
 
 
-<table>
 <form  action="wiederholung.php" method="post">
-
-<tr><td>Name </td><td><input name="nachname" type="text" value =""></td></tr>
-<tr><td>Vorname </td><td><input name="vorname" type="text" value =""></td></tr>
-<tr><td>Datum </td><td> <input name = "datum" type="text" value =""></td></tr>	
-<tr><td>Ausbilder</td>
-<td> <select name = "ausbilderId">
+<fieldset>
+<legend>Wiederholungsprüfung</legend>
+<p><label for="nachname">Name </label><input name="nachname" type="text" value =""></p>
+<p><label for="vorname">Vorname </label><input name="vorname" type="text" value =""></p>
+<p><label for="datum">Datum </label> <input name = "datum" type="text" value =""></p>	
+<p><label for="ausbilderId">Ausbilder</label><select name = "ausbilderId">
 <?php
 				$erg = get_ausbilder();
 				echo ausbilderSelector($erg);
 			 ?>
-</td>
-
-</tr>
-		
-<tr>
-	<td>	<?php if($_SESSION['usrlevel'] >= 2) {
-echo '<input  type="submit" name="submit" value="Absenden">';
+</p>
+		<?php if($_SESSION['usrlevel'] >= 2) {
+echo '<p><input  type="submit" name="submit" value="Absenden"></p>';
 }
 else {
 	echo 'Im Demomodus sind keine Eintragungen möglich.';
 } ?>
-		
-	</td>
-</tr>
-</form>
-</table>
 
-<hr>
+</fieldset>
+</form>
 
 

5926dfcf792a7153ed04392d8c5110af56e5e47c
Martin Roos <Martin.Roos@gmx.at>
2020-01-15 13:08:19 +0100

    removed codesmell

--- statistik.php
+++ statistik.php
@@ -22,123 +22,124 @@ if ( logged_in() ) {
 Bitte w&auml;hlen sie f&uuml;r eine Jahresstatistik das entsprechende Jahr und dr&uuml;cken sie "Abfragen"!
 
 
-<table>
-<form  action="statistik.php" method="post">
 
-<tr><td>Kursjahr </td><td> <select name = "jahr">
+<form  action="statistik.php" method="post">
+<fieldset>
+<legend>Jahresstatistik gesamt</legend>
+<p><label for="jahr">Kursjahr </label> <select name = "jahr">
 	
 				<?php
 				$erg = jahreszahlen();
 				echo kursjahrSelector($erg);
 			 ?>
-	</td>
-</tr>	
+	<select>
+</p>	
 
 		
-<tr>
-	<td>
+
 		<?php if($_SESSION['usrlevel'] >= 1) {
-echo '<input  type="submit" name="submit" value="Abfragen">';
+echo '<p><input  type="submit" name="submit" value="Abfragen"></p>';
 }
 else {
 	echo 'Im Demomodus sind keine Abfragen möglich.';
 } ?>
-	</td>
-</tr>
+	
+</fieldset>
 </form>
-</table>
+
 <hr>
 Bitte w&auml;hlen sie f&uuml;r eine Jahresstatistik das entsprechende Jahr und den OV / LV  und dr&uuml;cken sie "Abfragen"!
 
 
-<table>
-<form  action="statistik.php" method="post">
 
-<tr><td>Kursjahr </td><td> <select name = "jahr">
+<form  action="statistik.php" method="post">
+<fieldset>
+<legend>Jahresstatistik f&uuml;r einen LV/OV</legend>
+<p><label for="jahr">Kursjahr </label> <select name = "jahr">
 	
 				<?php
 				$erg = jahreszahlen();
 				echo kursjahrSelector($erg);
 			 ?>
-	</td>
-</tr>	
-<tr><td>LV / OV </td><td> <select name = "lvov">
+	</select>
+</p>	
+<p><label for="lvov">LV / OV </label> <select name = "lvov">
 	
 				<?php
 				$erg = lvov();
 				echo lvOvSelector($erg);
 			 ?>
-	</td>
-</tr>	
+	</select>
+</p>	
 
 		
-<tr>
-	<td><?php if($_SESSION['usrlevel'] >= 1) {
-echo '<input  type="submit" name="go" value="Abfragen">';
+<?php if($_SESSION['usrlevel'] >= 1) {
+echo '<p><input  type="submit" name="go" value="Abfragen"></p>';
 }
 else {
 	echo 'Im Demomodus sind keine Abfragen m&ouml;glich.';
 } ?>
 		
-	</td>
-</tr>
+	</fieldset>
 </form>
-</table>
 
 <hr>
 
 
 
 <?php 
+    $trtd = "<tr><td>";
+	$tdetd = "</td><td>";
+	$tdetre = "</td></tr>";
 
 if(isset($_POST['submit']))
 {
-echo "Statistik f&uumlr das Jahr ".$_POST['jahr'];
-echo "<table><tr><td>Abnahmen pro Kurs</td><td width=30></td><td>Abnahmen nach Level</td></tr><tr><td valign=\"top\">";
+	
+	
+echo "<table><caption>Statistik f&uuml;r das Jahr ".$_POST['jahr']." </caption><tr><th scope=\"col\">Abnahmen pro</th><td width=30></td><th scope =\"col\">gesamt Abnahmen</th></tr><tr><td valign=\"top\">";
 	$erg = get_pruefungen($_POST['jahr']);
 
 	$x = count($erg);
 	$i = 0;
 
-echo "<table>";
+echo "<table><caption>Kurs</caption>";
 	while($i<$x)
 	{
-		echo "<tr><td>".$erg[$i]."</td><td>".$erg[$i+1]."</td><tr>";
+		echo $trtd.$erg[$i].$tdetd.$erg[$i+1].$tdetre;
 		$i = $i+2;
 
 	}
-echo "</table> </td><td></td><td valign=\"top\">";
+echo "</table> ".$tdetd."</td><td valign=\"top\">";
 
 
 	$erg = get_statistik($_POST['jahr']);
 
 	$x = count($erg);
 	$i = 0;
-echo "<table>";
+echo "<table><caption>Ausbildungslevel</caption>";
 	while($i<$x)
 	{
-		echo "<tr><td>".$erg[$i]."</td><td>".$erg[$i+1]."</td><tr>";
+		echo $trtd.$erg[$i].$tdetd.$erg[$i+1].$tdetre;
 		$i = $i+2;
 
 	}
-echo "</table></td></tr></table>";
+echo "</table>".$tdetre."</table>";
 
 }
 if(isset($_POST['go']))
 {
 
-echo "Statistik f&uuml;r das Jahr ".$_POST['jahr']." und den LV / OV ".$_POST['lvov'];
 
-echo "<table><tr><td>Abnahmen pro Kurs</td><td width=30></td><td>Abnahmen nach Level</td></tr><tr><td valign=\"top\">";
+echo "<table><caption>Statistik f&uuml;r das Jahr ".$_POST['jahr']." und den LV / OV ".$_POST['lvov']."</caption><tr><th scope=\"col\">Abnahmen pro</th><td width=30></td><th scope=\"col\">gesamt Abnahmen</th></tr><tr><td valign=\"top\">";
 	$erg = get_pruefungen_lvov($_POST['jahr'],$_POST['lvov']);
 
 	$x = count($erg);
 	$i = 0;
 
-echo "<table>";
+echo "<table> <caption>Kurs</caption>";
 	while($i<$x)
 	{
-		echo "<tr><td>".$erg[$i]."</td><td>".$erg[$i+1]."</td><tr>";
+		echo $trtd.$erg[$i].$tdetd.$erg[$i+1].$tdetre;
 		$i = $i+2;
 
 	}
@@ -149,10 +150,10 @@ echo "</table> </td><td></td><td valign=\"top\">";
 
 	$x = count($erg);
 	$i = 0;
-echo "<table>";
+echo "<table><caption>Ausbildungslevel</caption>";
 	while($i<$x)
 	{
-		echo "<tr><td>".$erg[$i]."</td><td>".$erg[$i+1]."</td><tr>";
+		echo $trtd.$erg[$i]."</td><td>".$erg[$i+1]."</td><tr>";
 		$i = $i+2;
 
 	}
